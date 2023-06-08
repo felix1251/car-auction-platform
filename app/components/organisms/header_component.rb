86a1478aebf5ee139e0d2aa5e6c5aa2e8bc62526
felix1251 <domhack123@gmail.com>
2023-06-08 15:11:38 +0800

    implement auth

--- app/components/organisms/header_component.rb
+++ app/components/organisms/header_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Organisms::HeaderComponent < ViewComponent::Base
-
+    delegate :current_user, :user_signed_in?, :new_user_session_path,
+    :new_user_session_path, :new_user_registration_path, to: :helpers
 end
