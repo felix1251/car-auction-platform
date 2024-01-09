7cf3e0fb8b1cefd7f0fbdfef4a099c70a50aa9fe
felix1251 <domhack123@gmail.com>
2024-01-09 09:26:08 +0800

    ..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
     before_action :configure_sign_up_params, only: [:create], if: -> { devise_controller? }
+    skip_before_action :verify_authenticity_token
 
     private
 
